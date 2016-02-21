5ba000b1ed1ef91ec818d83af3dd56f6a4fdc310
shaharkadmiel <kadmiel@post.bgu.ac.il>
2016-02-21 12:58:42 +0200

    make OpenCV import optional

--- pySW4/utils/utils.py
+++ pySW4/utils/utils.py
@@ -14,7 +14,7 @@ try:
     import cv2
 except ImportError:
     warnings.warn('OpenCV not found, you will not be able to use '
-                  'pySW4.utils.resample` in `draft` mode.')
+                  '`pySW4.utils.resample` in `draft` mode.')
 from matplotlib.colors import LinearSegmentedColormap, ColorConverter
 
 def resample(data, extent, shape=None, method='linear',
